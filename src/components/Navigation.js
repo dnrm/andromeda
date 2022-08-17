110590b26bca105693ab0a6f56f800b3a8697ce1
Daniel Medina <daniel@medina.com>
2022-08-17 10:09:05 -0500

    :bug: Fixed navbar colors in product pages

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,6 @@ const Navigation = () => {
   const { cart } = useCartContext();
 
   useEffect(() => {
-    console.log(location.pathname);
     setCurrentLocation(location.pathname);
   }, [location]);
 
@@ -18,7 +17,7 @@ const Navigation = () => {
     <>
       <div
         className={`${
-          currentLocation === "/"
+          currentLocation === "/" || currentLocation.includes("/p/")
             ? "bg-black text-white"
             : "bg-white text-black"
         } w-full flex justify-between items-center tracking-tighter bg-black p-3 text-snow font-space-grotesk md:px-16 z-50 transition-all duration-200`}
@@ -109,7 +108,7 @@ const Navigation = () => {
       {navbarOpen && (
         <div
           className={`mobile-navbar ${
-            currentLocation === "/"
+            currentLocation === "/" || currentLocation.includes("/p/")
               ? "bg-black text-white"
               : "bg-white text-black"
           }`}
